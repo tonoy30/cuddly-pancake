e6abb8c291737eb164931678be2b348e5e04bd5f
tonoy30 <tonoy.sust@gmail.com>
2022-06-08 00:09:56 +0600

    no message

--- src/axiosClient.js
+++ src/axiosClient.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
-
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 const axiosClient = axios.create({
 	baseURL: `https://rentalpotential-octo-garbanzo.herokuapp.com/`,
-	withCredentials: true,
-	headers: {
-		'Access-Control-Allow-Origin': '*',
-	},
 });
 export { axiosClient };
