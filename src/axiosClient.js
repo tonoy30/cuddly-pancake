2c11b2da7073ba41109cdbcde90fe7964567c614
tonoy30 <tonoy.sust@gmail.com>
2022-06-07 21:36:39 +0600

    cors

--- src/axiosClient.js
+++ src/axiosClient.js
@@ -3,5 +3,8 @@ import axios from 'axios';
 const axiosClient = axios.create({
 	baseURL: `https://rentalpotential-octo-garbanzo.herokuapp.com/`,
 	withCredentials: true,
+	headers: {
+		'Access-Control-Allow-Origin': '*',
+	},
 });
 export { axiosClient };
