dca73b589ec8442e3b4559782d21704af9272941
tonoy30 <tonoy.sust@gmail.com>
2022-06-07 21:33:20 +0600

    axios cleint config

--- src/axiosClient.js
+++ src/axiosClient.js
@@ -2,5 +2,6 @@ import axios from 'axios';
 
 const axiosClient = axios.create({
 	baseURL: `https://rentalpotential-octo-garbanzo.herokuapp.com/`,
+	withCredentials: true,
 });
 export { axiosClient };
