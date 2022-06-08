9972db4a75d47d7cfaf46fbd586356722dfa3da2
tonoy30 <tonoy.sust@gmail.com>
2022-06-08 13:08:57 +0600

    base url fixed

--- src/axiosClient.js
+++ src/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-	baseURL: `https://rentalpotential-octo-garbanzo.herokuapp.com/v1/`,
+	baseURL: `https://potential-octo-garbanzo.herokuapp.com/v1/`,
 });
 export { axiosClient };
